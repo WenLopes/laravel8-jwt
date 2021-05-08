0cc6a01003bb8c8f711269318810e1a3e28ecbb5
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-08 15:45:35 -0300

    Removing not used middleware from auth constructor

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -8,16 +8,6 @@ use App\Http\Controllers\Controller;
 class AuthController extends Controller
 {
     /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login']]);
-    }
-
-    /**
      * Get a JWT via given credentials.
      *
      * @return \Illuminate\Http\JsonResponse
